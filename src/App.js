7600ef7a299d51448d873cd44f590d2b405b5d4b
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-01 10:51:33 -0500

    History API passed down through props with Route

--- src/App.js
+++ src/App.js
@@ -12,6 +12,17 @@ function Contact(props) {
     return <h1>Contact Component</h1>;
 }
 
+// History props example
+function Blog(props) {
+    const { push } = props.history;
+    return (
+        <div>
+            <h3>My blog nobody reads</h3>
+            <button onClick={() => push("/")}>Back home please!</button>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div className="App">
@@ -20,10 +31,12 @@ function App() {
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
                     <Link to="/contact">Contact</Link>
+                    <Link to="/blog">Blog</Link>
 
                     <Route exact path="/" component={Home} />
                     <Route path="/about" component={About} />
                     <Route path="/contact" component={Contact} />
+                    <Route path="/blog" component={Blog} />
                 </BrowserRouter>
             </header>
         </div>
