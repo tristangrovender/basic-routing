bb10eb068ff90a72463f3ec09efe3fd6c5de83a7
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-01 10:30:23 -0500

    very basic routing complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./App.css";
+
+function Home(props) {
+    return <h1>Home Component</h1>;
+}
+function About(props) {
+    return <h1>About Component</h1>;
+}
+function Contact(props) {
+    return <h1>Contact Component</h1>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <BrowserRouter>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                </BrowserRouter>
+            </header>
+        </div>
+    );
 }
 
 export default App;
